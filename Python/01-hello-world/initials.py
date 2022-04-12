0555265056fab41655322717b5ee2628da7138be
ImMichaelHughes <88868107+ImMichaelHughes@users.noreply.github.com>
2022-04-12 17:05:55 -0600

    Moved to 01-hello-world

---
+++ Python/01-hello-world/initials.py
@@ -0,0 +1,14 @@
+# This is my block art initials!
+# Insert fun fact here.
+
+print(
+"""
+M       M   H     H
+M M   M M   H     H
+M  M M  M   H     H
+M   M   M   HHHHHHH
+M       M   H     H
+M       M   H     H
+M       M   H     H
+"""
+)
